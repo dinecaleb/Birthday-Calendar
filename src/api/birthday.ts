6de000df9fe216068395da56253c364de3e35f5e
dinecaleb <calebtonyenwin@rocketmail.com>
2023-01-17 21:36:17 -0500

    remove junk

--- src/api/birthday.ts
+++ src/api/birthday.ts
@@ -1,5 +1,3 @@
-export const POKEMON_LIMIT = 10; //change this to up the values returned
-
 
 export const fetchBirthdays = async (month:Number,day:Number) => {
     let url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/${month}/${day}`;
